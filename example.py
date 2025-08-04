241020a3a671a5cfed2027b92ddff310e5e52a62
MCDev30 <charbelzeusmamlankou@gmail.com>
2025-08-04 22:56:24 +0100

    fixed issues

--- example.py
+++ example.py
@@ -5,7 +5,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "src"
 from mamnatroot import MamNatRootSolver
 
 def f(x):
-    return (x + 3) * (x - 1)
+    return (x + 3) * (x - 1)**3
 
 
 roots = MamNatRootSolver.find_all_roots(
